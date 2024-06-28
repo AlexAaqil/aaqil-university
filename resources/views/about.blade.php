81bbd9e6e8156f17bcbc4a7286cc8f6759c02cd9
alexaaqil <alexaaqil.se@gmail.com>
2024-06-28 22:50:23 +0300

    about page: added some content for the about page.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,6 +6,39 @@
         </div>
     </section>
 
+    <section class="About_details">
+        <div class="container">
+            <p>It seems like every other day, there's a new technology to learn, tool to master and endless articles to read. Honestly, it can be overwhelming and intimidating. So whether you're new or just need some guidance and support, Aaqil University is here to help.</p>
+
+            <h2>Why choose us?</h2>
+            <p>We just strive to function as a catalyst and challenge you to grow as both a developer and a human. We care about the details and will encourage you to think, plan and dream.</p>
+            
+
+            <h2>Benefits</h2>
+            <div class="benefits">
+                <div class="benefit">
+                    <p>Resources</p>
+                    <p>We'll point you towards informative content to learn from.</p>
+                </div>
+
+                <div class="benefit">
+                    <p>Networking</p>
+                    <p>You'll get connected with other pros to expand your reach.</p>
+                </div>
+
+                <div class="benefit">
+                    <p>Opportunity</p>
+                    <p>You'll be introduced and helped to source potential work.</p>
+                </div>
+
+                <div class="benefit">
+                    <p>Growth</p>
+                    <p>We'll uncover blindspots and accelerate your growth.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <section class="Team">
         <div class="container">
             <h1>Team</h1>
