ff5b6c5557433fd9189ef1974b61f5bb46ace8c1
alexaaqil <alexaaqil.se@gmail.com>
2025-11-26 12:01:38 +0300

    added the count for all users.

--- app/Livewire/Pages/Dashboard/Admin.php
+++ app/Livewire/Pages/Dashboard/Admin.php
@@ -20,6 +20,7 @@ class Admin extends Component
         $count_super_admins = User::where('role', USER_ROLES::SUPER_ADMIN)->count();
         $count_admins = User::where('role', USER_ROLES::ADMIN)->count();
         $count_users = User::where('role', '!=', USER_ROLES::SUPER_ADMIN)->count();
+        $count_all_users = User::count();
 
         $count_messages = ContactMessage::count();
         $count_unread_messages = ContactMessage::where('is_read', false)->count();
@@ -32,6 +33,7 @@ class Admin extends Component
             'count_super_admins',
             'count_admins',
             'count_users',
+            'count_all_users',
 
             'count_messages',
             'count_unread_messages',
