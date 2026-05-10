ef6ac22d667949508cf8c21abd79b645d1ca13c8
alexaaqil <alexaaqil.se@gmail.com>
2026-05-10 12:31:27 +0300

    updated the favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="shortcut icon" href="{{ asset('assets/images/aaqil-university-v2.png') }}" type="image/x-icon">
+
         {{-- CSRF --}}
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="shortcut icon" href="{{ asset('assets/images/aaqil-university-v2.png') }}" type="image/x-icon">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
